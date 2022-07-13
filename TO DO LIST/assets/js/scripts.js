f856b96041cd3652b8bc742f0da358e24d32f7c5
danmger <cavalcantedanilo51@gmail.com>
2022-07-13 15:23:47 -0300

    adicionando atividades DIO

---
+++ TO DO LIST/assets/js/scripts.js
@@ -0,0 +1,14 @@
+var btnSubmit = document.getElementById("btnSubmit").onclick = function() {
+}
+
+var text = document.getElementById("text")
+
+function Adicionar Tarefa() {
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function decrement() {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
